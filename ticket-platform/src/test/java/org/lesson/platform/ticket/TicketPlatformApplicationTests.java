139183448b2cf4435bffa591f2f5b6d9c03f107c
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-10-02 11:21:50 +0200

    add : project initialization

---
+++ ticket-platform/src/test/java/org/lesson/platform/ticket/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.platform.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
